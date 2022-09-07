8d0ac012ca2c75082fa210296f276d757216b587
S.Anvari <S@123456$>
2022-09-07 18:01:25 +0430

    information

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     COMMISSION_BASE_URL:'http://cluster.tech1a.co:9007/api',
-    NETFLOW_BASE_URL:'http://cluster.tech1a.co:8091/api',
+    NETFLOW_BASE_URL:'http://cluster.tech1a.co:8091',
   },
 }
 
