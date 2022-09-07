036ee5553d887e5a2a76301e636a59d885a87022
S.Anvari <S@123456$>
2022-09-07 17:19:39 +0430

    remove extra

--- components/account/ResultTable.tsx
+++ components/account/ResultTable.tsx
@@ -1,20 +1,11 @@
-
 import React, { useState, useEffect, useRef } from 'react';
-import { classNames } from 'primereact/utils';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-import { ProductService } from '../ProductService';
 import { Toast } from 'primereact/toast';
 import { Button } from 'primereact/button';
-import { FileUpload } from 'primereact/fileupload';
-import { Rating } from 'primereact/rating';
 import { Toolbar } from 'primereact/toolbar';
-import { InputTextarea } from 'primereact/inputtextarea';
-import { RadioButton } from 'primereact/radiobutton';
-import { InputNumber } from 'primereact/inputnumber';
 import { Dialog } from 'primereact/dialog';
-import { InputText } from 'primereact/inputtext';
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {getCommission} from "../../api/useCommission";
 import {Chip} from "primereact/chip";
 
