68c7e6cd4dd4f8cdbe0a36c43a4f8d2f027fa150
S.Anvari <S@123456$>
2023-07-23 18:03:10 +0330

    all allowed

--- components/common/functions/permission-utils.tsx
+++ components/common/functions/permission-utils.tsx
@@ -1,7 +1,7 @@
 export const isAllowed = ({ userPermissions, whoIsAllowed }: { userPermissions: string[], whoIsAllowed: string[] | undefined }) => {
     if (whoIsAllowed !== undefined && whoIsAllowed.length > 0) {
         let isAllowed = whoIsAllowed?.some((p: string) => userPermissions.indexOf(p) >= 0)
-        return isAllowed
+        return true
     }
     return true
 }
